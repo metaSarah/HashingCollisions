227759b85dd9f14e2a7273f6b247919727ee68f0
metaSarah <sarah@metaverse.enterprises>
2021-05-25 13:27:59 -0400

    hash_collision update

--- hash_collision.py
+++ hash_collision.py
@@ -1,6 +1,14 @@
 import hashlib
 import os
 
+import random
+
+""" hash_collision(k) returns x and y,
+such that the SHA256(x) and SHA256(y) match on their final k bits. 
+
+The return variables, x and y, should be encoded as bytes.
+"""
+
 def hash_collision(k):
     if not isinstance(k,int):
         print( "hash_collision expects an integer" )
@@ -10,9 +18,30 @@ def hash_collision(k):
         return( b'\x00',b'\x00' )
    
     #Collision finding code goes here
-    x = b'\x00'
-    y = b'\x00'
     
-    return( x, y )
+    # generate random var x
+    x = random.getrandbits(k) # Returns a non-negative integer with k random bits.
+    x_utf = str(x).encode('utf-8') # convert x to utf-8 byte format
+    x_sha = hashlib.sha256(x_utf).hexdigest()
+    
+    
+    # x = b'\x00'
+    
+    y_sha = None
+    y_utf = None
+    
+    for i in range(10000000):
+        y_utf = str(i).encode('utf-8')
+        result = hashlib.sha256(y_utf).hexdigest()
+        
+        if result[-k:] == x_sha[-k:]:
+            y_sha = result
+            break
+    
+    return x_utf, y_utf
 
-# test commit
+
+x, y = hash_collision(5)
+print(x)
+print(y)
+# print(chr(65)) # prints 'A'
